865c03ccd81aa2875ad5343907a5b5ec961cf985
sheilambadi <mbadiatieno@gmail.com>
2018-03-02 15:00:45 +0300

    final

--- app/src/main/java/com/wit/android/fikamarket/MainActivity.java
+++ app/src/main/java/com/wit/android/fikamarket/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wit.android.fikamarket;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.wit.android.fikamarket.vendors.VendorsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        /*Intent i = new Intent(this, FarmerActivity.class);
+        startActivity(i);*/
+
+        Intent i = new Intent(this, VendorsActivity.class);
+        startActivity(i);
+
+
     }
 }
