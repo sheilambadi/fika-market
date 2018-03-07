75840bc05cba914086d4c0571268a8a71050fe4b
sheilambadi <mbadiatieno@gmail.com>
2018-03-07 11:13:30 +0300

    farmer stock php file add

---
+++ farmer_stock.php
@@ -0,0 +1,29 @@
+<?php
+    $server = "localhost";
+    $db = "wit";
+    $db_username = "root";
+    $db_password="";
+
+    // Create connection
+    $conn = new mysqli($server, $db_username, $db_password, $db);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = "SELECT quantity, location, preferred_price, description FROM farmerstock";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while($row[] = $result->fetch_assoc()){
+            $tem = $row;
+            $json = json_encode($tem);
+         }
+    } else {
+        echo "0 results";
+    }
+    echo $json;
+
+    $conn->close();
+   ?>
